0a939d726ba9b09572ed9677187a2454369bac29
Rudolf Cicko <rudolfcicko2016@gmail.com>
2019-01-31 23:42:58 +0100

    MPA-002: Removing pods content

--- ios/Pods/Target Support Files/Pods-myPlacesApp/Pods-myPlacesApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_myPlacesApp : NSObject
-@end
-@implementation PodsDummy_Pods_myPlacesApp
-@end
--- ios/Pods/Target Support Files/Pods-myPlacesAppTests/Pods-myPlacesAppTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_myPlacesAppTests : NSObject
-@end
-@implementation PodsDummy_Pods_myPlacesAppTests
-@end
